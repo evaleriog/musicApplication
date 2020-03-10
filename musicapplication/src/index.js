49583bcd84cfd7d976d6c2e984cafa53b2e37bf1
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-03-10 15:48:13 -0500

    Connecting database to React and display albums

--- musicapplication/src/index.js
+++ musicapplication/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {AlbumsContainer} from './AlbumsContainer';
 
-const myFirstElement = <h1 className="dislay-4">Welcome to Tempo</h1>
-ReactDOM.render(myFirstElement, document.getElementById('root'));
+class Index extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            albums: []
+        }
+    }
+
+    render() {
+        return(
+            <div className="card-deck">
+                <AlbumsContainer />
+            </div>
+
+        );
+    }
+}
+// const myFirstElement = <h1 className="dislay-4">Welcome to Tempo</h1>
+ReactDOM.render(<Index />, document.getElementById('albums'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/main/java/com/application/music/repositories/AlbumRepository.java
+++ src/main/java/com/application/music/repositories/AlbumRepository.java
@@ -2,6 +2,8 @@ package com.application.music.repositories;
 
 import com.application.music.models.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 }
