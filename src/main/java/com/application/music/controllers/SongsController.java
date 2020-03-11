33c1d3cac528b9b05a8cd0071c4b4a21808840fb
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-03-11 16:29:13 -0500

    Adding functionality to controller

--- src/main/java/com/application/music/controllers/SongsController.java
+++ src/main/java/com/application/music/controllers/SongsController.java
@@ -3,6 +3,9 @@ package com.application.music.controllers;
 import com.application.music.models.Album;
 import com.application.music.models.Song;
 import com.application.music.repositories.AlbumRepository;
+import com.application.music.repositories.ArtistRepository;
+import com.application.music.repositories.PlaylistRepository;
+import com.application.music.repositories.SongRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +16,15 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 public class SongsController {
     private AlbumRepository albums;
+    private ArtistRepository artists;
+    private PlaylistRepository playlists;
+    private SongRepository songs;
 
-    public SongsController(AlbumRepository albums){
+    public SongsController(AlbumRepository albums, ArtistRepository artists, PlaylistRepository playlists, SongRepository songs){
         this.albums = albums;
+        this.artists = artists;
+        this.playlists = playlists;
+        this.songs = songs;
     }
 
     @GetMapping("/albums")
@@ -27,4 +36,12 @@ public class SongsController {
     List<Song> viewAllSongsPerAlbum(@PathVariable long id) {
         return albums.getOne(id).getSongs();
     }
+
+    @GetMapping("/artist/id/albums")
+    List<Album> viewAllAlbumsPerArtist(@PathVariable long id){
+
+        return albums.findAll();
+    }
+
+
 }
