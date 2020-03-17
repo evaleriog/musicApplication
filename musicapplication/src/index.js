0d9576b70a929013eef9617f3a23ae2ed5cb1bfc
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-03-17 18:23:48 -0500

    Adding Routing to React application

--- musicapplication/src/index.js
+++ musicapplication/src/index.js
@@ -1,25 +1,49 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import {WelcomePage} from './WelcomePage.js';
+// import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import {Albums} from "./Albums";
+//
+// class Index extends React.Component{
+//     constructor(props){
+//         super(props);
+//
+//         this.state = {
+//             albums: []
+//         }
+//     }
+//
+//     render() {
+//         return (
+//                 <Router>
+//                     <WelcomePage />
+//
+//                     <Switch>
+//                         <Route exact path="/" render={(props) => (
+//                             <WelcomePage />
+//                         )}/>
+//                         <Route exact path="/albums" component={Albums} />
+//
+//                     </Switch>
+//                 </Router>
+//             )
+//
+//     }
+// }
+// // const myFirstElement = <h1 className="dislay-4">Welcome to Tempo</h1>
+// ReactDOM.render(<Index />, document.getElementById('albums'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// // serviceWorker.unregister();
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import {WelcomePage} from './WelcomePage.js';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class Index extends React.Component{
-    constructor(props){
-        super(props);
+ReactDom.render(<App />, document.getElementById('albums'));
 
-        this.state = {
-            albums: []
-        }
-    }
-
-    render() {
-        return <WelcomePage />
-    }
-}
-// const myFirstElement = <h1 className="dislay-4">Welcome to Tempo</h1>
-ReactDOM.render(<Index />, document.getElementById('albums'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
